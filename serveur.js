b530383a1159d5b3bd04ce6956c69bd53e17a848
Loïc Fuentes <fuentes.loic@gmail.com>
2014-12-16 22:48:52 +0100

    Node.js test - init + first step

---
+++ serveur.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (page == '/') {
+        res.write('Vous êtes sur la page d\'acceuil');
+        res.end();
+    }
+    else if (page !== '/') {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.write('Méga bug');
+        res.end();    
+    }
+    
+});
+server.listen(8080);
